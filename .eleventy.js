f29059866a6eb4add94eb3fcb0eab3d2b36fd20a
Bader Alsabah <bader@badercodes.com>
2026-03-03 16:20:52 +0300

    standardized date formatting

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addFilter("limit", (arr, limit) => arr.slice(0, limit));
+  eleventyConfig.addFilter("date", (date, format) => {
+    const d = new Date(date);
+    return d.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+  });
 
   return {
     markdownTemplateEngine: "njk",
